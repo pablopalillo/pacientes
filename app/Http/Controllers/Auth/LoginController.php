904e021c9b9bd98bb3579ff6a99f14689b5b343f
pablopalillo <pablo.576@hotmail.com>
2018-12-17 22:04:17 -0500

    login y logout implementado

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use App\User;
 
 class LoginController extends Controller
 {
@@ -62,13 +63,18 @@ class LoginController extends Controller
                 if (Auth::user()) {
                     $userAuth = Auth::user();
 
-                    /** Respuesta para usuario valido */
+                    // Token de usuario 
+                    $userAuth->remember_token = str_random(60);
+                    $userAuth->save();
+
+                    // Respuesta para usuario valido 
                     $response["response"] = 1;
                     $response["code"] = 200;
 
-                    $response["id"] = $userAuth->id;
-                    $response["name"] = $userAuth->name;
-                    $response["email"] = $userAuth->email;
+                    $response["id"]     = $userAuth->id;
+                    $response["name"]   = $userAuth->name;
+                    $response["email"]  = $userAuth->email;
+                    $response["token"]  = $userAuth->remember_token;
                 
                     return response()->json($response);
                 } 
@@ -92,4 +98,54 @@ class LoginController extends Controller
             return response()->json($response);
         }
     }
+
+    /**
+     * Logout
+     * token: campo Remember_token guardado para cada usuario
+     * 
+     * Por defecto existe un metodo Auth:logout, pero solo para web.
+     * Para que funcione como API, debemos sobreescribir el comportamiento
+     * del metodo login. 
+     */
+
+     public function logout(Request $request)
+     {
+
+        $apiToken = $request->input('token');
+
+        $response = array(
+            "response" => 0,
+            "code" => null,
+            "error" => null
+        );
+
+        try {
+            if (!empty($apiToken)) {
+
+                $loggedUser =  User::where("remember_token", $apiToken)->first();
+
+                if($loggedUser) {
+
+                    $loggedUser->remember_token = null;
+
+                    $response["response"] = 1;
+                    $response["code"] = 200;
+                    return response()->json($response);
+                }   
+            }
+
+            $response["response"] = 0;
+            $response["code"] = 400;
+            $response["error"] = "usuario no encontrado";
+
+        } catch (Exception $e) {
+            $response["response"] = 0;
+            $response["error"] = "Server error";
+            $response["code"] = 500;
+            
+            return response()->json($response);
+        }
+
+     }
+
 }
